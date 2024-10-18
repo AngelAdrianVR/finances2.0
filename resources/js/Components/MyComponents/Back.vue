e9bf83ce5aa0ce5a6f761664d16ebbbb857a37ea
Angel Vazquez <angelvazquez470@gmail.com>
2024-10-17 18:29:35 -0600

    modificando sidenav

---
+++ resources/js/Components/MyComponents/Back.vue
@@ -0,0 +1,26 @@
+<template>
+  <button @click="handleRedirection()"
+    class="flex justify-center items-center rounded-full size-9 focus:outline-none hover-3dbuttom z-50">
+    <i class="fa-solid fa-angle-left text-black text-base"></i>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      default: null
+    }
+  },
+  methods: {
+    handleRedirection() {
+      if (this.to) {
+        this.$inertia.visit(this.to);
+      } else {
+        window.history.back();
+      }
+    },
+  },
+};
+</script>
